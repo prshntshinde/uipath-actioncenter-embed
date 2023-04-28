a9c0100fb3b6f37747b885502a3e526a79c8d4f4
Prashant Shinde <mr.pshinde@gmail.com>
2023-04-28 09:13:40 +0000

    Feat: Embed ActionCenter in 3rd party tool

---
+++ my-app/pages/index.js
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <embed src="https://cloud.uipath.com/embed_/prashant_orch/Dev_Instance/actions_/tasklist/tasks/142283" width="1280" height="720" />
+}
+
+export default HomePage
